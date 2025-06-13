0a1c2bdfc5bcf4159439fcbc7425076bad12b450
Abdulrahman Hussin <abdelrahman.hussin1@gmail.com>
2025-06-14 01:51:15 +0300

    first commit

---
+++ database/migrations/2025_06_13_223347_create_properties_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('properties', function (Blueprint $table) {
+            $table->id();
+            $table->string('title',255);
+            $table->integer('land_area');
+            $table->enum('hangar_type', ['hangar', 'truss'])->nullable();
+            $table->integer('hangar_area')->nullable();
+            $table->decimal('hangar_height', 5, 2)->nullable();
+            $table->integer('admin_floors')->nullable();
+            $table->decimal('electricity_power', 5, 2)->nullable();
+            $table->enum('electricity_unit',['kw','mega'])->nullable();
+            $table->date('license_expiry_date')->nullable();
+
+            $table->string('location')->nullable();
+
+            $table->integer('cranes_count')->default(0);
+            $table->decimal('price', 15, 2)->nullable();
+            $table->text('notes')->nullable();
+            $table->foreignId('property_type_id')->constrained('property_types')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreignId('created_by')->nullable()->constrained('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreignId('updated_by')->nullable()->constrained('users')->onDelete('cascade')->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('properties');
+    }
+};
